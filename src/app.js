925b8cae3208ef35c181636f03b8c5f642f28d16
zonghangli <lizhuestc@gmail.com>
2026-09-01 13:09:24 +0400

    Publish verified Hy4 STQ deployment guidance

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { presetConversations } from "./preset-conversations.js?v=appended-presets-v2-20260823";
-import { hy4ReplayData } from "./hy4-replay-data.js?v=hy4-prima-098-v11-20260828";
+import { hy4ReplayData } from "./hy4-replay-data.js?v=hy4-stq1-replay-v13-20260901";
 import { renderMarkdownInto } from "./markdown-renderer.js";
 
 document.documentElement.classList.add("js");
